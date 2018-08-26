7882e0ed2593a2d10396f39b30a9340f291e8f0a
ficorrea <fisrael.correa@gmail.com>
2018-08-26 18:26:53 -0300

    arquivos curso

---
+++ 03 - Classificação/regressão_logística.py
@@ -0,0 +1,13 @@
+# Regressão Logística
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
