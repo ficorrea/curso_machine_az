dc117294f817fd6c2ada725d280f08c9a136f281
ficorrea <fisrael.correa@gmail.com>
2018-09-09 21:07:24 -0300

    arquivos curso

--- 08 - Deep Learning/cnn.py
+++ 08 - Deep Learning/cnn.py
@@ -7,17 +7,20 @@ from keras.layers import Flatten
 from keras.layers import Dense
 
 classifier = Sequential()
-classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu')) # Parâmetro input_shape no keras é invertido
+# Parâmetro input_shape no keras é invertido
+classifier.add(Convolution2D(
+    32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Camada adicionada para melhorar acurácia
-classifier.add(Convolution2D(32, 3, 3, activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
+""" classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2))) """
 
 classifier.add(Flatten())
 classifier.add(Dense(output_dim=128, activation='relu'))
 classifier.add(Dense(output_dim=1, activation='sigmoid'))
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+classifier.compile(
+    optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 from keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(rescale=1./255,
@@ -37,7 +40,7 @@ test = test_datagen.flow_from_directory('dataset/test_set',
                                         batch_size=32,
                                         class_mode='binary')
 
-classifier.fit_generator(train, 
+classifier.fit_generator(train,
                          steps_per_epoch=8000,
                          epochs=25,
                          validation_data=test,
